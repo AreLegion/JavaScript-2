816173e20115aa5bb281ceb28acff196d70e0691
DostoN <Dostoon@gmail.com>
2022-11-18 21:13:34 +0300

    js And Html

---
+++ script.js
@@ -0,0 +1,49 @@
+var modal = prompt ( "Ismingizni Kiriting...")
+console.log("Sizning Ismingiz: "+modal);
+var age = +prompt ("Yoshingizni Kiriting....")
+console.log("Sizning Yoshingiz: "+age);
+
+
+
+var misol = +prompt (" Misolni Yeching... 7+8=?")
+if (misol ==15){
+    console.log("Sizning Javobingiz To'g'ri: ✔️"+misol);
+}
+else{
+    console.log("Sizning Javob NoTo'g'ri: "+misol+" ❌|   👉🏻  :To'g'ri Javob 15 ✔️|");
+}
+
+var misol = +prompt (" Misolni Yeching... 15-8=?")
+if (misol ==7){
+    console.log("Sizning Javobingiz To'g'ri: ✔️"+misol);
+}
+else{
+    console.log("Sizning Javob NoTo'g'ri: "+misol+" ❌|   👉🏻  :To'g'ri Javob 7 ✔️|");
+}
+
+var misol = +prompt (" Misolni Yeching... 30*10=?")
+if (misol ==300){
+    console.log("Sizning Javobingiz To'g'ri: ✔️"+misol);
+}
+else{
+    console.log("Sizning Javob NoTo'g'ri: "+misol+" ❌|   👉🏻  :To'g'ri Javob 300 ✔️|");
+}
+
+var misol = +prompt (" Misolni Yeching... 30/10=?")
+if (misol ==3){
+    console.log("Sizning Javobingiz To'g'ri: ✔️"+misol);
+}
+else{
+    console.log("Sizning Javob NoTo'g'ri: "+misol+" ❌|   👉🏻  :To'g'ri Javob 3 ✔️|");
+}
+
+var misol = +prompt (" Misolni Yeching... 100%6=?")
+if (misol ==2){
+    console.log("Sizning Javobingiz To'g'ri: ✔️"+misol);
+}
+else{
+    console.log("Sizning Javob NoTo'g'ri: "+misol+" ❌|   👉🏻  :To'g'ri Javob 2 ✔️|");
+}
+alert("Konsolga Kirish Uchun Bosing...                                                       👇🏻")
+
+
